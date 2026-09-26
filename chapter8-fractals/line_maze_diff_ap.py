73f9cea40edcd448ae85739de6a0b1d8fd96fae9
Prolay Mondal <prolaymondal047@gmail.com>
2026-09-26 10:43:53 +0530

    chapter 8 update

--- chapter8-fractals/line_maze_diff_ap.py
+++ chapter8-fractals/line_maze_diff_ap.py
@@ -22,7 +22,7 @@ class Line():
         pygame.draw.line(self.screen,self.color,self.start,self.end)
 
 class Linegenerator():
-    def __init__(self,screen,depth,x1,y1,x2,y2):
+    def __init__(self,screen,depth,x1,y1,x2,y2,linelist):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -31,35 +31,41 @@ class Linegenerator():
         self.dy = self.y2 - self.y1
         self.screen= screen
         self.depth=depth
+        self.linelist = linelist
+        self.branch_gen()
 
 
     def branching(self):
         if self.dx == 0 and self.dy > 4:
-            return Linegenerator(self.screen,self.depth-1,self.x1 - self.dy / 3,self.y1,self.x1 + self.dy / 3,self.y1), Linegenerator(self.screen,self.depth-1,self.x1 - self.dy / 3,self.y2,self.x1 + self.dy / 3,self.y2)
+            return Linegenerator(self.screen,self.depth-1,self.x1 - self.dy / 3,self.y1,self.x1 + self.dy / 3,self.y1,self.linelist), Linegenerator(self.screen,self.depth-1,self.x1 - self.dy / 3,self.y2,self.x1 + self.dy / 3,self.y2,self.linelist)
         elif self.dy == 0 and self.dx > 4:
-            return Linegenerator(self.screen,self.depth-1,self.x1, self.y1 - self.dx / 3,self.x1, self.y1 + self.dx / 3), Linegenerator(self.screen,self.depth-1,self.x2, self.y1 - self.dx / 3,self.x2, self.y1 + self.dx / 3)
-    def draw_branch(self):
+            return Linegenerator(self.screen,self.depth-1,self.x1, self.y1 - self.dx / 3,self.x1, self.y1 + self.dx / 3,self.linelist), Linegenerator(self.screen,self.depth-1,self.x2, self.y1 - self.dx / 3,self.x2, self.y1 + self.dx / 3,self.linelist)
+    def branch_gen(self):
         if self.depth <= 0:
             return 
-        line1,line2 = self.branching()
-        line1.draw_branch()
-        line2.draw_branch()
         main_line = Line((self.x1 ,self.y1),(self.x2,self.y2),self.screen)
-        main_line.draw()
+        line1,line2 = self.branching()
+        self.linelist.append(main_line)
+        line1.branch_gen()
+        line2.branch_gen()
 
 
-line_gen = Linegenerator(screen,10,width / 4,height / 2,3 * width / 4,height / 2)
+linelist = []
+line_gen = Linegenerator(screen,10,width / 4,height / 2,3 * width / 4,height / 2,linelist)
 
 
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            line_gen.branch_gen()
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("Black")
 
-
-    line_gen.draw_branch()
+    #line_gen.branch_gen()
+    for line in line_gen.linelist:
+        line.draw()
 
     pygame.display.flip()
     clock.tick(FPS)
